0a02c5452e11328d9498083bbab8a31b432bbe8d
abhirup2565 <abhirup2565@gmail.com>
2025-07-14 12:01:17 +0530

    setting file set

--- warship.py
+++ warship.py
@@ -1,6 +1,7 @@
 import sys
-
 import pygame
+
+from settings import Settings
 
 class Warship:
     """overall class to manage game assests and behavior"""
@@ -8,9 +9,12 @@ class Warship:
     def __init__(self):
         """initialize the game and create resource"""
         pygame.init()
-        
-        self.screen=pygame.display.set_mode((1200,800))
+        self.Setting=Settings()
+        self.clock=pygame.time.Clock()
+
+        self.screen=pygame.display.set_mode((self.Setting.screen_width,self.Setting.screen_height))
         pygame.display.set_caption("Warship")
+        self.bg_color=self.Setting.bg_color
 
     def run_game(self):
         """Start main loop for the game """
@@ -19,8 +23,10 @@ class Warship:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             #make recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__=="__main__":
     ai=Warship()
