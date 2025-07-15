fb486ae8f6510a37cdccbc272b090c6c03a76936
abhirup2565 <abhirup2565@gmail.com>
2025-07-15 10:36:44 +0530

    added rows of aliens

--- warship.py
+++ warship.py
@@ -27,15 +27,23 @@ class Warship:
 
     def _create_fleet(self):
         alien=Alien(self)
-        alien_width=alien.rect.width
+        alien_width,alien_height=alien.rect.size
 
-        current_x=alien_width
-        while current_x<(self.Setting.screen_width-2*alien_width):
-            new_alien = Alien(self)
-            new_alien.x=current_x
-            new_alien.rect.x=current_x
-            self.aliens.add(new_alien)
-            current_x+=2*alien_width
+        current_x,current_y=alien_width,alien_height
+        while current_y<(self.Setting.screen_height-3*alien_height):
+            while current_x<(self.Setting.screen_width-2*alien_width):
+                self._create_alien(current_x,current_y)
+                current_x+=2*alien_width
+            current_x=alien_width
+            current_y+=2*alien_height
+
+    def _create_alien(self,x_positon,y_position):
+        new_alien = Alien(self)
+        new_alien.x=x_positon
+        new_alien.rect.x=x_positon
+        new_alien.rect.y=y_position
+        self.aliens.add(new_alien)
+
                     
     def _check_keydown_events(self,event):
         """respond to keypress"""
