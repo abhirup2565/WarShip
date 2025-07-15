d1b4aed520b3840e77372b5d58a37cf573596ed4
abhirup2565 <abhirup2565@gmail.com>
2025-07-15 12:26:06 +0530

    destroying aliens

--- warship.py
+++ warship.py
@@ -96,6 +96,7 @@ class Warship:
         for bullet in self.bullets.copy():
                 if bullet.rect.bottom<=0:
                     self.bullets.remove(bullet)   
+        collisions=pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
